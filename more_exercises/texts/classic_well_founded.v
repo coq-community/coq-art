a6f8825f2912a23fd3827db57ec7a5e56772ee20
Pierre Casteran <pierre.casteran@labri.fr>
2019-11-20 19:18:57 +0100

    creation of a directory of new exercises

---
+++ more_exercises/texts/classic_well_founded.v
@@ -0,0 +1,60 @@
+(**
+    The "classical" definition of well-foundedness
+ *)
+
+Require Import Relations.
+
+(** 
+  Please consider the usual mathematical definition of well-founded relations :
+ *)
+
+
+Definition classic_wf {A}(R: relation A) :=
+  ~ exists (s: nat-> A),  forall i,  R (s (S i)) (s i).
+
+(** Prove that Coq's  definition entails the classical one *)
+
+Theorem wf_classic_wf {A} (R: relation A) : well_founded R -> classic_wf R.
+Admitted.
+
+(** Now, we work with some axioms *)
+
+Require Import Classical ClassicalChoice.
+
+
+
+(** In the current context, prove that the classical definition entails Coq's 
+    (you may apply choice or Choice *)
+
+About choice.
+
+About Choice.
+
+
+Theorem classic_wf_wf {A} (R: relation A) : classic_wf R -> well_founded R.
+Admitted.
+  
+
+
+Print Assumptions classic_wf_wf.
+
+(*
+
+Axioms:
+relational_choice : forall (A B : Type) (R : A -> B -> Prop),
+                    (forall x : A, exists y : B, R x y) ->
+                    exists R' : A -> B -> Prop, subrelation R' R /\ (forall x : A, exists ! y : B, R' x y)
+dependent_unique_choice : forall (A : Type) (B : A -> Type) (R : forall x : A, B x -> Prop),
+                          (forall x : A, exists ! y : B x, R x y) ->
+                          exists f : forall x : A, B x, forall x : A, R x (f x)
+classic : forall P : Prop, P \/ ~ P
+
+*)
+
+Print Assumptions wf_classic_wf.
+
+(*
+
+Closed under the global context
+*)
+
